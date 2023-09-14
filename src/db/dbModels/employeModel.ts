a54ebcb1334168b571e73fd3a1b0f50d6a53536a
RodrigoSpano <rorrospano17@gmail.com>
2023-09-14 20:12:03 -0300

    feature: items and employes models

---
+++ src/db/dbModels/employeModel.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { EmployeBase } from "../../utils/interfaces/employes.interface";
+
+const EmployeSchema = new mongoose.Schema<EmployeBase>({
+  name: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: 17
+  },
+  position: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  hired_at: {
+    type: String,
+    required: true
+  },
+  nationality: {
+    type: String,
+    required: true
+  }
+})
+
+export default mongoose.model('employe', EmployeSchema)
---
+++ src/utils/interfaces/employes.interface.ts
@@ -0,0 +1,10 @@
+export interface EmployeBase {
+  id?: string
+  name: string
+  lastname: string
+  age: number
+  position: string
+  hired_at: string
+  gender: 'male' | 'female'
+  nationality: string
+} 
